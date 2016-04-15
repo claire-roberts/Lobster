01c81103dc1a5aeab8547262ca289f1b7dfbfb69
Claire Roberts <Claire@GMFMC.local>
2016-04-15 10:31:39 -0400

    added rectangles and accuracy calculator

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ shinyServer(function(input, output, session) {
       addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/Mapserver/tile/{z}/{y}/{x}',
                options = providerTileOptions(noWrap = TRUE)) %>%
       
-      setView(-81.41, 26.15, zoom = 8) %>% 
+      #setView(-81.41, 26.15, zoom = 8) %>% 
     addPolygons(data=lobs, stroke=FALSE, smoothFactor=0.2, fillOpacity=1.0,
                 color="yellow") 
     
@@ -31,6 +31,14 @@ shinyServer(function(input, output, session) {
     input$geolocation
   }) ## renderPrint
   
+  output$accuracy <- renderPrint({
+    input$accuracy
+  })
+  
+  output$time <- renderPrint({
+    input$time
+  })
+  
   ###observer to change map based on location##
   
   observe({
@@ -39,7 +47,17 @@ shinyServer(function(input, output, session) {
       dist <- 0.5
       lat <- input$lat
       lng <- input$long
-      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist)
+      map %>% fitBounds(lng - dist, lat - dist, lng + dist, lat + dist) %>% 
+      #addMarkers(-81.41, 26.15) %>% 
+        addMarkers(lng,lat) %>% 
+        addRectangles(lng1=  -82.517131, lat1=27.963717,
+                      lng2=-82.511541, lat2=27.959590,
+                      fillColor="#dd1c77", fillOpacity = 1,
+                      color = "#dd1c77") %>% 
+        addRectangles(lng1=  -82.635620, lat1=27.956195,
+                      lng2=-82.597855, lat2=27.932922,
+                      fillColor="#dd1c77", fillOpacity = 1,
+                      color = "#dd1c77")
     }
   })
   
--- ui.R
+++ ui.R
@@ -14,34 +14,126 @@ shinyUI(navbarPage("Lobster Closed Areas", id="nav",
                                 
                                 
                             ), ## div
-                            tags$script('
+    #                         tags$script('
+    #           $(document).ready(function () {
+    # #                                     navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    # #                                     
+    # #                                     function onError (err) {
+    # #                                     Shiny.onInputChange("geolocation", false);
+    # #                                     }
+    # #                                     
+    # #                                     function onSuccess (position) {
+    # #                                     setTimeout(function () {
+    # #                                     var coords = position.coords;
+    # #                                     console.log(coords.latitude + ", " + coords.longitude);
+    # #                                     Shiny.onInputChange("geolocation", true);
+    # #                                     Shiny.onInputChange("lat", coords.latitude);
+    # #                                     Shiny.onInputChange("long", coords.longitude);
+    # #                                     }, 1100)
+    # #                                     }
+    # #                                     });
+    #                                     '), ## tags$script
+    #                         tags$script('
+    #           var TIMEOUT = 1000; //SPECIFY
+    #           var started = false;
+    #                                     function getLocationRepeat(){
+    #                                     //first time only - no delay needed
+    #                                     if (!started) {
+    #                                     started = true;
+    #                                     getLocation(getLocationRepeat);
+    #                                     return;
+    #                                     }
+    #                                     
+    #                                     setTimeout(function () {
+    #                                     getLocation(getLocationRepeat);
+    #                                     }, TIMEOUT);
+    #                                     
+    #                                     };
+    #                                     
+    #                                     getLocationRepeat();
+    #                                     '),  
+    #                         tags$script('
+    #                                var options = {
+    #                                   enableHighAccuracy: true,
+    #                                   timeout: 5000,
+    #                                   maximumAge: 0
+    #                               };
+    #                                     '), 
+    # 
+    # # Show a plot of the generated distribution
+    # fluidRow(column(width = 2, 
+    #                 verbatimTextOutput("lat"),
+    #                 verbatimTextOutput("long"),
+    #                 verbatimTextOutput("geolocation"))
+    # ) ## fluid Row
+    
+    tags$script('
               $(document).ready(function () {
-                                        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-                                        
-                                        function onError (err) {
-                                        Shiny.onInputChange("geolocation", false);
-                                        }
-                                        
-                                        function onSuccess (position) {
-                                        setTimeout(function () {
-                                        var coords = position.coords;
-                                        console.log(coords.latitude + ", " + coords.longitude);
-                                        Shiny.onInputChange("geolocation", true);
-                                        Shiny.onInputChange("lat", coords.latitude);
-                                        Shiny.onInputChange("long", coords.longitude);
-                                        }, 1100)
-                                        }
-                                        });
-                                        '), ## tags$script
-
+                
+                function getLocation(callback){
+                var options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+                };
+                
+                navigator.geolocation.getCurrentPosition(onSuccess, onError);
+                
+                function onError (err) {
+                Shiny.onInputChange("geolocation", false);
+                }
+                
+                function onSuccess (position) {
+                setTimeout(function () {
+                var coords = position.coords;
+                var timestamp = new Date();
+                
+                console.log(coords.latitude + ", " + coords.longitude, "," + coords.accuracy);
+                Shiny.onInputChange("geolocation", true);
+                Shiny.onInputChange("lat", coords.latitude);
+                Shiny.onInputChange("long", coords.longitude);
+                Shiny.onInputChange("accuracy", coords.accuracy);
+                Shiny.onInputChange("time", timestamp)
+                
+                console.log(timestamp);
+                
+                if (callback) {
+                callback();
+                }
+                }, 1100)
+                }
+                }
+                
+                var TIMEOUT = 1000; //SPECIFY
+                var started = false;
+                function getLocationRepeat(){
+                //first time only - no delay needed
+                if (!started) {
+                started = true;
+                getLocation(getLocationRepeat);
+                return;
+                }
+                
+                setTimeout(function () {
+                getLocation(getLocationRepeat);
+                }, TIMEOUT);
+                
+                };
+                
+                getLocationRepeat();
+                
+                });
+                '),
+    
     # Show a plot of the generated distribution
-    fluidRow(column(width = 2, 
+    fluidRow(column(width = 2,
                     verbatimTextOutput("lat"),
                     verbatimTextOutput("long"),
-                    verbatimTextOutput("geolocation"))
-    ) ## fluid Row
-                  
-                   ) ## tabPanel
+                    verbatimTextOutput("geolocation"),
+                     verbatimTextOutput("accuracy")#,
+                    # verbatimTextOutput("time"))
+    )
+    ) ## tabPanel
   
             ) ## navbarPage
-)## ShinyUI
+))## ShinyUI
